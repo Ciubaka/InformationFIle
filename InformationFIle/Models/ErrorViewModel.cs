786172bab49ba6aedcc4a71df36c63f10871a55d
Ciubaka <ildefonsjarzyna@gmail.com>
2018-10-23 15:35:42 +0200

    Dodaj pliki projektów.

---
+++ InformationFIle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InformationFIle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
